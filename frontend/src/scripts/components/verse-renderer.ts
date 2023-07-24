f59dea0c4401760149a366f3ddd3de54fe8ca64a
Matthew Bernardo <matthew@e-bot7.com>
2023-07-24 13:35:38 +0200

    Support switching translations

--- frontend/src/scripts/components/verse-renderer.ts
+++ frontend/src/scripts/components/verse-renderer.ts
@@ -3,27 +3,49 @@ import { lilComponent } from "lil-framework"
 
 const name = "verse-renderer"
 
-const template = /*html*/``
+const template = /*html*/`
+<div class="version-wrapper">
+  Translation:&nbsp;
+  <input lfbind="version:input" style="font-size: 12pt;" list="suggested-translations" placeholder="Select translation">
+  <datalist id="suggested-translations">
+      <option value="ESV">
+      <option value="NIV">
+      <option value="NKJV">
+      <option value="NTV">
+      <option value="RVR1960">
+  </datalist>
+</div>
+<div class="links-wrapper"></div>
+`
 
 lilComponent({
   name,
   template,
   data: {
     verses: "[]",
+    version: "ESV"
   },
   hooks: {
     verses: [function(versesString: string) {
-      let verses = JSON.parse(versesString)
-      // TODO: Handle cases where verse references might contain multiple passages, e.g., John 1:1-2,4
-      verses = verses.reduce((acc: Array<string>, references: string) => {
-        return acc.concat(splitReferences(references))
-      }, [])
-      this.innerHTML = verses.map((v: string) => {
-        return /*html*/`<a target="_blank" href="${getBibleGatewayHref(v)}" class="verse chip">${v}</a>`
-      }).join("")
-    }]
+      this.style.display = "block"
+      this.querySelector(".links-wrapper").innerHTML = getVersesHtml(versesString, this.state.version)
+    }],
+    version: [function(version: string) {
+      this.querySelector(".links-wrapper").innerHTML = getVersesHtml(this.state.verses, version)
+    }] 
   }
 })
+
+function getVersesHtml(versesString: string, version: string = "ESV") {
+  let verses = JSON.parse(versesString)
+  // TODO: Handle cases where verse references might contain multiple passages, e.g., John 1:1-2,4
+  verses = verses.reduce((acc: Array<string>, references: string) => {
+    return acc.concat(splitReferences(references))
+  }, [])
+  return verses.map((v: string) => {
+    return /*html*/`<a target="_blank" href="${getBibleGatewayHref(v, version)}" class="verse chip">${v}</a>`
+  }).join("")
+}
 
 function getBibleGatewayHref (verse: string, version: string = "ESV") {
   // Here's an example of the link format.
